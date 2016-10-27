ebacf4f3ea18121bd2819bfbc788b712958ea54d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-10-27 16:27:11 +0200

    menu item

--- app/01_login/jlg-layout/jlg-layout.js
+++ app/01_login/jlg-layout/jlg-layout.js
@@ -3,6 +3,19 @@
 
 	var app = angular.module('jlg-layout', []);
 
+	app.directive('jlgLogo', function() {
+		return {
+			link: function(scope, element, attrs) {
+				element.html('<img src="' + attrs.src + '" />');
+			}
+		}
+	});
 
+	app.directive('jlgUtilitiesItem', function() {
+		return {
+			template: '<button class="btn btn-default btn-success" ng-transclude></button>',
+			transclude: true
+		}
+	});
 	
 })();
