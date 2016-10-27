d7160330602c498a902901601c961d6b43ec3d9b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-10-27 18:38:27 +0200

    compile for ng-click

--- app/01_login/jlg-layout/jlg-layout.js
+++ app/01_login/jlg-layout/jlg-layout.js
@@ -30,22 +30,27 @@
 
 	app.directive('jlgMenuItem', ['$injector', function($injector) {
 		return {
-			link: function(scope, element, attrs) {
-				console.log('link jlgMenu', arguments);
-				// look at the url and deduct which item is active
-				scope.$on('$routeChangeStart', function(event, next, current) {
-					console.log('$routeChangeStart', arguments);
+			compile: function(tElement, tAttrs, transclude) {
+				console.log('compile jlgMenuItem', arguments);
+				var href = tAttrs.href;
+				tElement.attr('ng-click', 'goto(\'' + href + '\')');
+				var link = function(scope, element, attrs) {
+					console.log('link jlgMenuItem', arguments);
+					// look at the url and deduct which item is active
+					scope.$on('$routeChangeStart', function(event, next, current) {
+						console.log('$routeChangeStart', arguments);
 
-					var toUrl = next.originalPath;
-					console.log('$routeChangeStart', toUrl);
-					if (toUrl === attrs.href) {
-						element.addClass('active');
-					} else {
-						element.removeClass('active');
-					}
-				});
+						var toUrl = next.originalPath;
+						console.log('$routeChangeStart', toUrl);
+						if (toUrl === attrs.href) {
+							element.addClass('active');
+						} else {
+							element.removeClass('active');
+						}
+					});
+				};
+				return link;
 			}
-
 		};
 	}]);
 })();
